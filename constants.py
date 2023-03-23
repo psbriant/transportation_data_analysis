89296008040728b68b505912547d775abe595dc5
psbriant <briantp@email.arizona.edu>
2023-03-23 14:06:32 -0700

    Added constant variables for creating a heartmap

--- constants.py
+++ constants.py
@@ -1,3 +1,16 @@
 """
 Description: File for storing recurring settings
 """
+
+from dataclasses import dataclass
+
+@dataclass
+class HeatmapArguments:
+    index: str = 'MONTH'
+    columns: str = 'YEAR'
+    values: str = 'AVG_RIDES'
+    output_file: str = 'ridership_heatmap.png'
+
+
+
+
