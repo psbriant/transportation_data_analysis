30c881a0d6d2bdb2b77b24166f487464597a6bb8
psbriant <briantp@email.arizona.edu>
2023-03-23 14:23:45 -0700

    Added code to create a heatmap

--- main.py
+++ main.py
@@ -28,11 +28,51 @@ if __name__ == "__main__":
         required=True,
         type=str,
         help='The absolute file path to the bus data being analyzed')
+    parser.add_argument(
+        '--output_dir',
+        required=True,
+        type=str,
+        help='The absolute file path to output directory where the plots will'
+             ' be saved')
 
     args = parser.parse_args()
 
     bus_data_path = args.bus_data_path
+    output_dir = args.output_dir
 
+    # ------------------------------------------------------------------------
+    # ---INITIALIZE CONSTANT ARGUMENTS----------------------------------------
+    # ------------------------------------------------------------------------
 
+    heatmap_args = HeatmapArguments()
 
+    # ------------------------------------------------------------------------
+    # ---LOAD DATASET---------------------------------------------------------
+    # ------------------------------------------------------------------------
 
+    logging.info("Loading data")
+    cta_bus_data = pd.read_csv(bus_data_path, encoding='utf-8')
+
+    # ------------------------------------------------------------------------
+    # ---CREATE HEATMAP FOR RIDERSHIP BY MONTH AND YEAR (1999-2022)-----------
+    # ------------------------------------------------------------------------
+    # Currently heatmaps are only for weekday ridership
+    # TODO: Determine how much of this code can be moved to functions in
+    # data_processing.py
+    # ------------------------------------------------------------------------
+
+    # Heatmap by weekday ridership, month and year.
+    hm_rmy_data = cta_bus_data.copy()
+    hm_rmy_data = hm_rmy_data[hm_rmy_data['ROUTE'] == '1']
+    hm_rmy_data = hm_rmy_data[hm_rmy_data['DAY_TYPE'] == 'Weekday']
+
+    # Create output path for heatmap
+    heatmap_output_path = f'{output_dir}{heatmap_args.output_file}'
+
+    # TODO figure out how to declare these variables using constants.py.
+    create_heatmap(
+        data=hm_rmy_data,
+        output_path=heatmap_output_path,
+        data_index=heatmap_args.index,
+        data_columns=heatmap_args.columns,
+        data_values=heatmap_args.values)
