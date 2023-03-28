900d1d9d57af65bea766f16f173a0e6365da32b1
psbriant <briantp@email.arizona.edu>
2023-03-28 13:09:52 -0700

    Added code for additional heatmaps

--- main.py
+++ main.py
@@ -12,7 +12,9 @@ import logging
 import numpy as np
 import pandas as pd
 
-from constants import (HeatmapArguments)
+from constants import (HeatmapArguments,
+                       HeatmapArguments_1999_2010,
+                       HeatmapArguments_2011_2022)
 from visualizations import (create_heatmap)
 
 
@@ -45,6 +47,8 @@ if __name__ == "__main__":
     # ------------------------------------------------------------------------
 
     heatmap_args = HeatmapArguments()
+    heatmap_args_1999_2010 = HeatmapArguments_1999_2010()
+    heatmap_args_2011_2022 = HeatmapArguments_2011_2022()
 
     # ------------------------------------------------------------------------
     # ---LOAD DATASET---------------------------------------------------------
@@ -75,6 +79,61 @@ if __name__ == "__main__":
         data_index=heatmap_args.index,
         data_columns=heatmap_args.columns,
         data_values=heatmap_args.values)
+
+    # ------------------------------------------------------------------------
+    # ---CREATE HEATMAP FOR RIDERSHIP BY MONTH AND YEAR (1999-2010)-----------
+    # ------------------------------------------------------------------------
+    # Currently heatmaps are only for weekday ridership
+    # TODO: Determine how much of this code can be moved to functions in
+    # data_processing.py
+    # ------------------------------------------------------------------------
+
+    # Heatmap by weekday ridership, month and year.
+    hm_rmy_data_1999_2010 = cta_bus_data.copy()
+    hm_rmy_data_1999_2010 = hm_rmy_data_1999_2010[
+        hm_rmy_data_1999_2010['ROUTE'] == '1']
+    hm_rmy_data_1999_2010 = hm_rmy_data_1999_2010[
+        hm_rmy_data_1999_2010['DAY_TYPE'] == 'Weekday']
+    hm_rmy_data_1999_2010 = hm_rmy_data_1999_2010[
+        hm_rmy_data_1999_2010['YEAR'] < 2011]
+
+
+    # Create output path for heatmap
+    heatmap_output_path = f'{output_dir}{heatmap_args_1999_2010.output_file}'
+
+    create_heatmap(
+        data=hm_rmy_data_1999_2010,
+        output_path=heatmap_output_path,
+        data_index=heatmap_args_1999_2010.index,
+        data_columns=heatmap_args_1999_2010.columns,
+        data_values=heatmap_args_1999_2010.values)
+
+    # ------------------------------------------------------------------------
+    # ---CREATE HEATMAP FOR RIDERSHIP BY MONTH AND YEAR (2011-2022)-----------
+    # ------------------------------------------------------------------------
+    # Currently heatmaps are only for weekday ridership
+    # TODO: Determine how much of this code can be moved to functions in
+    # data_processing.py
+    # ------------------------------------------------------------------------
+
+    # Heatmap by weekday ridership, month and year.
+    hm_rmy_data_2011_2022 = cta_bus_data.copy()
+    hm_rmy_data_2011_2022 = hm_rmy_data_2011_2022[
+        hm_rmy_data_2011_2022['ROUTE'] == '1']
+    hm_rmy_data_2011_2022 = hm_rmy_data_2011_2022[
+        hm_rmy_data_2011_2022['DAY_TYPE'] == 'Weekday']
+    hm_rmy_data_2011_2022 = hm_rmy_data_2011_2022[
+        hm_rmy_data_2011_2022['YEAR'] > 2010]
+
+    # Create output path for heatmap
+    heatmap_output_path = f'{output_dir}{heatmap_args_2011_2022.output_file}'
+
+    create_heatmap(
+        data=hm_rmy_data_2011_2022,
+        output_path=heatmap_output_path,
+        data_index=heatmap_args_2011_2022.index,
+        data_columns=heatmap_args_2011_2022.columns,
+        data_values=heatmap_args_2011_2022.values)
 
     # ------------------------------------------------------------------------
     # ---CREATE PLOTS FOR THE TOP TEN ROUTES BY SATURDAY RIDERSHIP IN 2022----
