cb0db1ee7e889d5299dac59a982221d79872e0ea
psbriant <briantp@email.arizona.edu>
2023-08-13 16:28:51 -0700

    Created ranking function

--- data_processing.py
+++ data_processing.py
@@ -62,3 +62,45 @@ def change_column_datatype(
         return updated_dfs
     else:
         return updated_dfs[0]
+
+
+def create_rankings(
+        df: pd.DataFrame,
+        value_col: str,
+        rank_col: str,
+        group_col: list[str],
+        num_rankings: int = 0) -> pd.DataFrame:
+    """
+    Ranks rows in a dataframe by a specified column.
+
+    Arguments:
+        df (DataFrame): Dataset to develop rankings for.
+        value_col (str): The name of the column rankings will be based off of.
+        rank_col (str): The name of the column containing the numerical
+            rankings.
+        group_col (strList): The columns used for ensuring rankings are made
+            across columns (e.g. specifying the year column in a dataset
+            containing bus routes, ridership numbers and years will rank each
+            bus route by ridership for each year).
+        num_rankings (int): The number of rows to return rankings for. If set
+            to zero, no limit will be applied and all rows will be ranked.
+            Defaults to zero.
+
+    Returns:
+        Dataframe with numerical rankings for each row.
+
+    Raises:
+        NONE
+    """
+
+    rank_df = df.copy()
+
+    # Add a rank column based off of the value of value_col.
+    rank_df[rank_col] = rank_df.groupby(group_col)[
+        value_col].rank(ascending=False)
+
+    # Subset dataframe by the value of limit. Do not specify if set to zero.
+    if num_rankings > 0:
+        rank_df = rank_df[rank_df[rank_col] <= num_rankings]
+
+    return rank_df
