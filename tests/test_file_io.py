3bddf61a6d64fbad58701238f300c56a2d2c6fb8
psbriant <briantp@email.arizona.edu>
2023-12-12 18:33:40 -0800

    Added a function to test value errors after more were added to create_absolute_file_paths to improve the reliability of the function

--- tests/test_file_io.py
+++ tests/test_file_io.py
@@ -77,3 +77,41 @@ def test_create_absolute_file_paths_type_exceptions(
         create_absolute_file_paths(
             file_list=file_list,
             file_path=file_path)
+
+
+@pytest.mark.parametrize(
+    "file_list,file_path,expected",
+    [([""], "/dir1/", "/dir1/file1.csv"),
+     (["file1"], "/dir1/", "/dir1/file1.csv"),
+     ([".csv"], "/dir1/", "/dir1/file1.csv"),
+     (["file1.csv"], "dir1/", "/dir1/file1.csv")])
+def test_create_absolute_file_paths_value_exceptions(
+        file_list: list[str],
+        file_path: str,
+        expected: list[str]) -> list[str] | str:
+    """
+    Tests the following:
+    1. Tests whether ValueErrors are raised if elements of 'file_list' do not
+        have file extensions.
+    2. Tests whether ValueErrors are raised if elements of 'file_name' do not
+        contain characters before the '.' of the file extension (e.g. '.csv')
+    3. Tests whether ValueErrors are raised if the value of variable
+        'file_path' does not begin with a '/'.
+
+    Arguments:
+        file_list (strList): List of files to join with a specified absolute
+            path.
+        file_path (str): The file path to join the files specified in
+            'file_list' to.
+        expected (StrList): The expected test case.
+
+    Returns:
+        NONE
+
+    """
+    with pytest.raises(ValueError):
+
+        create_absolute_file_paths(
+            file_list=file_list,
+            file_path=file_path)
+
