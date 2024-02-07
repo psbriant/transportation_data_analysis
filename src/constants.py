72575c9d3e9d1c3c4d4677c3cf89c946039d01b6
psbriant <briantp@email.arizona.edu>
2024-02-07 15:51:02 -0800

    Updated values to be in line with the current data format that CTA is using

--- src/constants.py
+++ src/constants.py
@@ -57,7 +57,7 @@ class BusDataArguments:
 
 @dataclass
 class BarChartArguments:
-    x_value: str = 'RIDES'
+    x_value: str = 'AVG_RIDES'
     y_value: str = 'ROUTE'
     x_value_type: str = 'quantitative'
     y_value_type: str = 'ordinal'
@@ -75,7 +75,7 @@ class BumpChartArguments:
     title: str = "Chicago Transit Authority bus routes with the highest " \
                  "number of riders (2001-2022)"
     scheme: str = 'category20'
-    value_col: str = 'RIDES'
+    value_col: str = 'AVG_RIDES'
     rank_col: str = 'RANK'
     group_col: list[str] = field(default_factory=lambda: ['YEAR'])
     num_rankings: int = 10
@@ -83,14 +83,14 @@ class BumpChartArguments:
 @dataclass
 class LineChartArguments:
     x_value: str = 'YEAR'
-    y_value: str = 'RIDES'
+    y_value: str = 'AVG_RIDES'
     x_value_type: str = 'ordinal'
     y_value_type: str = 'quantitative'
     color_values: str = 'ROUTE'
     title: str = "Chicago Transit Authority bus routes with the highest " \
                  "number of riders (2001-2022)"
     scheme: str = 'category20'
-    value_col: str = 'RIDES'
+    value_col: str = 'AVG_RIDES'
     rank_col: str = 'RANK'
     group_col: list[str] = field(default_factory=lambda: ['YEAR'])
     num_rankings: int = 10
@@ -101,7 +101,7 @@ class HeatmapArguments:
     x_value_type: str = 'ordinal'
     y_value: str = 'YEAR'
     y_value_type: str = 'ordinal'
-    color_values: str = 'RIDES'
+    color_values: str = 'AVG_RIDES'
     facet_values: str = 'ROUTE'
     facet_columns: int = 3
     scheme: str = 'yelloworangebrown'
