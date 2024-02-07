768416d0fa35cbb9557cc240b185171ff9b7d973
psbriant <briantp@email.arizona.edu>
2024-02-07 12:14:43 -0800

    Removed and updated code used for processing the pervious data format that CTA was using

--- src/constants.py
+++ src/constants.py
@@ -36,50 +36,24 @@ viz_file_names = {
 
 @dataclass
 class Months(TypedDict):
-   name_numeric: str
+   name_numeric: int
    name_alpha: str
 
 @dataclass
-class DateTypes(TypedDict):
-    long_numeric: str
-    short_numeric: str
-
-@dataclass
-class DayTypes(TypedDict):
-   type_sort: str
-   type_long: str
-
-@dataclass
 class BusDataArguments:
-    sort_form_to_long_form_numeric_dates: DateTypes = field(
-        default_factory=lambda: {
-            '01': '1',
-            '02': '2',
-            '03': '3',
-            '04': '4',
-            '05': '5',
-            '06': '6',
-            '07': '7',
-            '08': '8',
-            '09': '9'})
     alpha_to_numeric_months: Months = field(default_factory=lambda: {
-        '1': 'January',
-        '2': 'February',
-        '3': 'March',
-        '4': 'April',
-        '5': 'May',
-        '6': 'June',
-        '7': 'July',
-        '8': 'August',
-        '9': 'September',
-        '10': 'October',
-        '11': 'November',
-        '12': 'December'})
-    sort_form_to_long_form_day_types: DayTypes = field(
-        default_factory=lambda: {
-            'W': 'Weekday',
-            'A': 'Saturday',
-            'U': 'Sunday - Holiday'})
+        1: 'January',
+        2: 'February',
+        3: 'March',
+        4: 'April',
+        5: 'May',
+        6: 'June',
+        7: 'July',
+        8: 'August',
+        9: 'September',
+        10: 'October',
+        11: 'November',
+        12: 'December'})
 
 @dataclass
 class BarChartArguments:
