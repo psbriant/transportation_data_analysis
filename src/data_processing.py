9dffd85473241ef73fb561d154588298f3ecf0f2
psbriant <briantp@email.arizona.edu>
2024-02-25 21:29:17 -0800

    Added new functionality to do mean aggregations

--- src/data_processing.py
+++ src/data_processing.py
@@ -180,7 +180,8 @@ def subset_dataframes_by_value(
 def aggregate_data(
         df: pd.DataFrame,
         agg_cols: list[str],
-        id_cols: list[str]):
+        id_cols: list[str],
+        agg_type: str) -> pd.DataFrame:
     """
     # Create aggregate ridership data by route, year for each service type
 
@@ -188,17 +189,30 @@ def aggregate_data(
         df (DataFrame): Pandas dataframe to aggregate.
         agg_cols (strList): The column to aggregate the data by.
         id_cols (strList): The columns representing non-aggregated dimensions.
+        agg_type (str): The type of aggregation to perform on the data. Must
+            be one of either 'sum' or 'mean'.
 
     Returns:
         Dataframe that has been aggregated by the specified dimensions.
 
     Raises:
-        NONE
+        ValueError if agg_type is not one of 'sum' or 'mean'.
     """
 
     agg_df = df.copy()
     agg_df = agg_df.drop(columns=agg_cols)
-    agg_df = agg_df.groupby(by=id_cols).sum()
+
+    if agg_type == 'sum':
+        agg_df = agg_df.groupby(by=id_cols).sum()
+
+    elif agg_type == 'mean':
+        agg_df = agg_df.groupby(by=id_cols).mean()
+
+    else:
+        raise ValueError(
+            f"Unsupported agg_type of {agg_type}, please use either 'sum' "
+            f"or 'mean'")
+
     agg_df = agg_df.reset_index()
 
     return agg_df
